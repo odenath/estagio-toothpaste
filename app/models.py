2bd8a91c0efb019eb3114435de882628e2e62c0f
Gustavo <gustavo.odenath@edu.unifil.br>
2023-06-02 16:20:01 -0300

    Pequena refatoração

--- app/models.py
+++ app/models.py
@@ -6,20 +6,12 @@ from django.db import models
 
 # Create your models here.
 class Person(User, models.Model):
-
-    """
-    #TODO: Editar a classe padrão de usuário
-    """
-
     GENDER_CHOICES = [
         ("M", "Male"),
         ("F", "Female"),
         ("O", "Other")
     ]
-
-    __tablename__ = 'tb_user'    
-
-
+    __tablename__ = 'tb_user'
     birthdate = models.DateTimeField(null=False)
     gender = models.CharField(
         max_length=1,
@@ -30,22 +22,18 @@ class Person(User, models.Model):
 
     def __repr__(self) -> str:
         return f'<Usuario {self.first_name}, {self.last_name}>'
-    
+
 
 class Dentist(Person, models.Model):
-
     __tablename__ = "tb_dentist"
-
     cro_number = models.CharField(null=False, max_length=8)
     uf_cro = models.CharField(null=False, max_length=2)
     cnpj = models.CharField(max_length=14, null=False)
     legal_name = models.CharField(max_length=40, null=False)
-    
+
 
 class Address(models.Model):
-
     __tablename__ = "tp_address"
-
     user = models.ForeignKey(Person, on_delete=models.CASCADE)
     cep = models.CharField(max_length=8, null=False)
     street_avenue = models.CharField(max_length=40, null=False)
@@ -56,15 +44,13 @@ class Address(models.Model):
 
     def __repr__(self) -> str:
         return f'<Addrress {self.city}, {self.uf}>'
-    
-    
+
+
 class Phone(models.Model):
     __tablename__ = "tb_phone"
-
     user = models.ForeignKey(Person, on_delete=models.CASCADE)
     ddd = models.CharField(null=False, max_length=2)
     number = models.CharField(null=False, max_length=9)
 
     def __repr__(self) -> str:
         return f"<Phone ({self.ddd}){self.number}>"
-    
